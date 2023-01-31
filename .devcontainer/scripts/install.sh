8bdc5c5b48ecaf1c4e22e2d98bcc3ff508d9e409
Joe McConnell <joe.mcconnell10@gmail.com>
2023-01-31 09:53:29 -0600

    working on file permissions

--- .devcontainer/scripts/install.sh
+++ .devcontainer/scripts/install.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-touch test.out
+apt-get update 
+apt install gradle -f
+apt install wget -f
+apt install git -f
+apt install default-jdk -f
+./gradlew installRoboRioToolchain
+cd /opt
+wget 'https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2023.2.1/vscode-wpilib-2023.2.1.vsix'
+code --install-extension vscode-wpilib-2023.2.1.vsix 
