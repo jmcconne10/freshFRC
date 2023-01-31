af9abb11b7074b5621a0251aebdfbf555a6ec260
Joe McConnell <joe.mcconnell10@gmail.com>
2023-01-31 14:29:10 -0600

    fixed gradlew

--- devContainer-scripts/install.sh
+++ devContainer-scripts/install.sh
@@ -5,7 +5,7 @@ apt install wget -y
 apt install git -y
 apt install default-jdk -y
 cd ..
-./gradlew installRoboRioToolchain
+../gradlew installRoboRioToolchain
 cd /opt
 wget 'https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2023.2.1/vscode-wpilib-2023.2.1.vsix'
 code --install-extension vscode-wpilib-2023.2.1.vsix 
