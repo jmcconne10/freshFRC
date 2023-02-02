ef2e1b3592c44c1dece80920d3b9d9417e1b7212
Joe McConnell <joe.mcconnell10@gmail.com>
2023-02-02 14:17:28 -0600

    troubleshoot

--- devContainer-scripts/install.sh
+++ devContainer-scripts/install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd ..
-./gradlew installRoboRioToolchain > output.txt
+#./gradlew installRoboRioToolchain > output.txt
+pwd > /opt/output.txt
 wget 'https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2023.2.1/vscode-wpilib-2023.2.1.vsix'
 mv vscode-wpilib-2023.2.1.vsix /opt
 code --install-extension vscode-wpilib-2023.2.1.vsix 
