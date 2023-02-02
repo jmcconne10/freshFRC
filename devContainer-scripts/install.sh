2f3ba41e682695c0d64dff7732a762dcbb8a6a6e
Joe McConnell <joe.mcconnell10@gmail.com>
2023-02-02 13:55:06 -0600

    moved to install.sh

--- devContainer-scripts/install.sh
+++ devContainer-scripts/install.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
-apt-get update 
-apt install gradle -y
-apt install wget -y
-apt install git -y
-apt install default-jdk -y
 cd ..
 ./gradlew installRoboRioToolchain
-cd /opt
 wget 'https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2023.2.1/vscode-wpilib-2023.2.1.vsix'
+mv vscode-wpilib-2023.2.1.vsix /opt
 code --install-extension vscode-wpilib-2023.2.1.vsix 
